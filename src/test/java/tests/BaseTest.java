bec14a9998c4ebe376252cc1bedae5d8091e5280
Olya Chistikova <olya.chistikova@mail.ru>
2025-11-06 13:28:29 +0300

    Add BaseTest class for common test setup

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
+
+@Getter
+public class BaseTest {
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        if (driver != null){
+            driver.quit();
+        }
+    }
+}
