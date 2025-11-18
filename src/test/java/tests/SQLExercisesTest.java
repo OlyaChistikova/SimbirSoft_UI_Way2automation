cb5fa76ecfeb2d9c94599b8f36c9c4c911a549dd
Olya Chistikova <olya.chistikova@mail.ru>
2025-11-18 21:09:10 +0300

    Added SQLExercisesTest

---
+++ src/test/java/tests/SQLExercisesTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import data.InputData;
+import helpers.CookieUtils;
+import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.SQLExercisesPage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SQLExercisesTest extends BaseTest{
+    private WebDriver driver;
+    private SQLExercisesPage sqlExercisesPage;
+
+    @BeforeMethod(description = "Открываем страницу Banking App")
+    public void setUrl() {
+        driver = getDriver();
+        sqlExercisesPage = new SQLExercisesPage(driver);
+    }
+
+    @Test(description = "Проверка авторизации и сохранения cookie")
+    @Epic("Авторизация пользователя")
+    @Feature("Работа с cookie")
+    @Story("Сохранение cookie авторизированного пользователя")
+    @Severity(SeverityLevel.CRITICAL)
+    public void authorisationAndSaveCookiesTest()  {
+        sqlExercisesPage.setLoginField(InputData.loginSQLAuthorisation)
+                .setPasswordField(InputData.passwordRegistration)
+                .clickSubmitButton();
+        Assert.assertTrue(sqlExercisesPage.checkVisibilityLogoutImg());
+        CookieUtils.saveCookiesToFile(driver, InputData.getCookieFilePath());
+
+        Path path = Paths.get(InputData.getCookieFilePath());
+        Assert.assertTrue(Files.exists(path), "Cookie file was not saved successfully.");
+    }
+
+    @Test(description = "Проверка авторизации с использованием сохраненных cookie")
+    @Epic("Авторизация пользователя")
+    @Feature("Работа с cookie")
+    @Story("Авторизация с использованием сохраненных cookies пользователя")
+    @Severity(SeverityLevel.CRITICAL)
+    public void authorisationBySavedCookiesTest() {
+        driver.manage().deleteAllCookies();
+        CookieUtils.loadCookiesFromFile(driver, InputData.getCookieFilePath());
+        driver.navigate().refresh();
+        Assert.assertTrue(sqlExercisesPage.checkVisibilityLogoutImg());
+    }
+}
