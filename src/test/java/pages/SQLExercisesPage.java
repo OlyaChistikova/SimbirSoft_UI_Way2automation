2efb04e75b33b6e12ed4f03afe18cf4576e6c045
Olya Chistikova <olya.chistikova@mail.ru>
2025-11-19 22:03:48 +0300

    Added focus and scroll method to SQLExercisesPage

--- src/test/java/pages/SQLExercisesPage.java
+++ src/test/java/pages/SQLExercisesPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import helpers.JavaScriptExecutorHelper;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,41 +11,128 @@ import static data.EndPoint.SQL;
 
 public class SQLExercisesPage {
     private WebDriver driver;
+    JavaScriptExecutorHelper jsHelper;
     private static final String URL = SQL.getUrl();
 
+    /**
+     * Локатор поля ввода логина
+     */
     @FindBy(xpath = "//input[@name='login' and @type='text']")
     private WebElement loginField;
 
+    /**
+     * Локатор поля ввода пароля
+     */
     @FindBy(xpath = "//input[@name='psw' and @type='password']")
     private WebElement passwordField;
 
+    /**
+     * Локатор кнопки входа
+     */
     @FindBy(xpath = "//input[@value='Вход']")
     private WebElement submitButton;
 
+    /**
+     * Локатор кнопки выхода (logout)
+     */
     @FindBy(xpath = "//a[@href='/logout.php']")
     private WebElement logoutButton;
 
+    /**
+     * Конструктор страницы SQL упражнений
+     *
+     * @param driver WebDriver
+     */
     public SQLExercisesPage(WebDriver driver) {
         this.driver = driver;
         driver.get(URL);
         PageFactory.initElements(driver, this);
+        jsHelper = new JavaScriptExecutorHelper(driver);
     }
 
+    /**
+     * Установка значения в поле логина
+     *
+     * @param login значение логина
+     * @return текущий объект страницы для цепочки вызовов
+     */
+    @Step("Задаем логин в поле логина")
     public SQLExercisesPage setLoginField(String login) {
         loginField.sendKeys(login);
         return this;
     }
 
+    /**
+     * Установка значения в поле пароля
+     *
+     * @param password значение пароля
+     * @return текущий объект страницы для цепочки вызовов
+     */
+    @Step("Задаем пароль в поле пароля")
     public SQLExercisesPage setPasswordField(String password) {
         passwordField.sendKeys(password);
         return this;
     }
 
-    public void clickSubmitButton(){
+    /**
+     * Нажатие на кнопку входа
+     */
+    @Step("Кликаем по кнопке входа")
+    public void clickSubmitButton() {
         submitButton.click();
     }
 
-    public boolean checkVisibilityLogoutImg(){
+    /**
+     * Проверка отображения элемента выхода (иконки logout)
+     *
+     * @return true, если элемент отображается
+     */
+    @Step("Проверяем отображение кнопки выхода (logout)")
+    public boolean checkVisibilityLogoutImg() {
         return logoutButton.isDisplayed();
+    }
+
+    /**
+     * Клик по полю логина (для фокусировки)
+     */
+    @Step("Кликаем по полю логина")
+    public void clickLoginField() {
+        loginField.click();
+    }
+
+    /**
+     * Сброс фокуса с поля логина с помощью JavaScript
+     */
+    @Step("Сбрасываем фокус с поля логина с помощью JavaScript")
+    public void resetFocusFromField() {
+        jsHelper.resetFocusFromField(driver, loginField);
+    }
+
+    /**
+     * Проверка, что фокус сброшен с поля логина
+     *
+     * @return true, если фокус сброшен
+     */
+    @Step("Проверяем, что фокус сброшен с поля логина")
+    public boolean checkResetFocus() {
+        return jsHelper.checkResetFocusFromField(driver, loginField);
+    }
+
+    /**
+     * Скролл страницы до нижней части
+     */
+    @Step("Прокручиваем страницу до нижней части")
+    public void scrollToButton() {
+        jsHelper.scrollToBottom();
+    }
+
+    /**
+     * Проверка вертикальной прокрутки страницы
+     *
+     * @return true, если страница прокручена
+     */
+    @Step("Проверяем вертикальную прокрутку страницы")
+    public boolean checkVerticalScroll() {
+        return jsHelper.checkVerticalScroll(driver);
     }
 }
