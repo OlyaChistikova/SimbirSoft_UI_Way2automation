1847dac9d543d7275cc69722f668b32c19eb4044
Olya Chistikova <olya.chistikova@mail.ru>
2025-11-23 16:56:16 +0300

    Reformat BaseTest for using DriverFactory

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,21 +1,17 @@
 package tests;
 
 import data.InputData;
+import helpers.DriverFactory;
 import helpers.JavaScriptExecutorHelper;
 import io.qameta.allure.Step;
 import lombok.Getter;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
-import java.net.URL;
 import java.time.Duration;
 
 @Getter
@@ -26,7 +22,7 @@ public class BaseTest {
 
     @BeforeMethod(description = "Настройка браузера перед запуском тестов", enabled = true)
     public void setUp(){
-        driver = createDriver();
+        driver = DriverFactory.createDriver(InputData.getBrowser());
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -35,20 +31,13 @@ public class BaseTest {
 
     @BeforeMethod(description = "СетАп для параллельного тестирования", enabled = false)
     public void parallelSetUp() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setPlatform(Platform.WIN11);
-        capabilities.setBrowserName("chrome");
         try{
-            driver = new RemoteWebDriver(new URL(InputData.getGridHubURL()), capabilities);
+            driver = DriverFactory.createGridDriver(InputData.getBrowser());
         } catch (IOException ex){
             ex.printStackTrace();
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-    }
-
-    protected WebDriver createDriver() {
-        return new ChromeDriver();
     }
 
     @Step("Делаем скриншот в случае провала теста")
