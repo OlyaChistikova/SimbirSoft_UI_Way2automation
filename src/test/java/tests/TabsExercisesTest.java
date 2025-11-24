d6587deacaea98cdedffcb26fce54ed3115febf1
Olya Chistikova <olya.chistikova@mail.ru>
2025-11-24 18:51:03 +0300

    Added TabsExercisesTest

---
+++ src/test/java/tests/TabsExercisesTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.TabsExercisesPage;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class TabsExercisesTest extends BaseTest {
+    private WebDriver driver;
+    TabsExercisesPage tabsExercisesPage;
+
+    @BeforeMethod(description = "Открываем страницу Frames And Windows")
+    public void setUrl() {
+        driver = getDriver();
+        tabsExercisesPage = new TabsExercisesPage(driver);
+    }
+
+    @Test(description = "Проверка переключения фокуса между вкладками")
+    @Epic("Работа с вкладками и окнами браузера")
+    @Feature("Переключение и управление вкладками")
+    @Story("Переключение фокуса между вкладками")
+    @Severity(SeverityLevel.CRITICAL)
+    public void switchFocusBetweenTabsTest() {
+        tabsExercisesPage.switchToFrame();
+        tabsExercisesPage.switchToTab();
+        tabsExercisesPage.switchToTab();
+
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> iterator = handles.iterator();
+
+        driver.switchTo().window(tabsExercisesPage.getBrowserTabByNumber(handles, iterator, driver.getWindowHandles().size()));
+
+        softAssert.assertEquals(driver.getWindowHandles().size(), 3);
+        softAssert.assertEquals(driver.getCurrentUrl(), "http://way2automation.com/way2auto_jquery/frames-windows/defult1.html#");
+        softAssert.assertAll();
+    }
+}
